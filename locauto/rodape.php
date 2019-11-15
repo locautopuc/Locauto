b02d6eca8065debfc71e99198e31b8a87333ba4c
locautopuc <57334380+locautopuc@users.noreply.github.com>
2019-11-15 07:48:49 -0300

    Add files via upload

---
+++ locauto/rodape.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+<div class="container has-text-centered ">
+    <p>
+		<strong>Locauto</strong> por <a href=""> Giulianno, Romario, Vinicius e Guilherme</a>.
+      <a href="">PUC GO</a>. Direitos reservados <a href="">Versão 1.0</a>.
+    </p>
+  </div>
+</footer>
+
