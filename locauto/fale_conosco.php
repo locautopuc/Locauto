98714e4466fc8d05dcb3ea18153c972f146d1789
Locauto <57334380+locautopuc@users.noreply.github.com>
2019-11-15 17:01:11 -0300

    Update fale_conosco.php

--- locauto/fale_conosco.php
+++ locauto/fale_conosco.php
@@ -11,66 +11,62 @@
     <link rel="stylesheet" href="css/bulma.min.css" />
     <link rel="stylesheet" type="text/css" href="css/config.css">
 </head>
-
-
 <body>
 	
-	
 	<?php 
-	// chamando o arquivo de cabeçalho
-	require 'cabecalho.php'; 
+		// chamando o arquivo de cabeçalho
+		require 'cabecalho.php'; 
 	?>
 	
     <section class="hero is-success is-fullheight">
         <div class="hero-body">
-			<div class="container has-text-centered">
-                <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-grey">Locauto<strong>®</strong></h3>
-                    <h3 class="title has-text-grey"><a href="" target="_blank"> </a></h3>
+		<div class="container has-text-centered">
+                	<div class="column is-4 is-offset-4">
+                    		<h3 class="title has-text-grey">Locauto<strong>®</strong></h3>
+                    		<h3 class="title has-text-grey"><a href="" target="_blank"> </a></h3>
 					
 					
 					<!-- Interface -->
-                    <div class="box"><strong>Fale Conosco</strong>
-						<br><br>
+      <div class="box"><strong>Fale Conosco</strong><br><br>
 							
-						<form action="contato.php" method="POST" autocomplete="off">
-                            <div class="field">
-                                <div class="control">
-                                    <strong>Nome:</strong>
-									<input name="nome" name="text" class="input is-large" placeholder="Nome" autofocus oninput="this.value = this.value.replace(/[^A-Za-z0-9]+/g, '').replace(/(\..*)\./g, '$1');">
-                                </div>
-                            </div>
-							<div class="field">
-                                <div class="control">
-                                    <strong>Seu Email:</strong>
-                                    <input name="email" class="input is-large" type="text" placeholder="Seu E-mail">
-								 </div>
-                            </div>
-							<div class="field">
-                                <div class="control">
-                                    <strong>Comentário:</strong>
-                                    <textarea style="height:10em;" name="comentario" class="input is-large" type="text" placeholder="Seu comentario" oninput="this.value = this.value.replace(/[^A-Za-z0-9]+/g, '').replace(/(\..*)\./g, '$1');"></textarea>
-								 </div>
-                            </div>
-							<div class="field">
-                                <a href="index.php"><strong>Cancelar</strong></a>
-                            </div>
-							<!-- Botão entrar com design CSS -->
-                            <button type="submit" class="button is-dark is-block  is-large is-fullwidth">Enviar</button>
-                        </form>
-						
-						
-						
-						
-						<br><br>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+	<form action="contato.php" method="POST" autocomplete="off">
+		
+		<div class="field">
+			<div class="control">
+				<strong>Nome:</strong>
+				<input name="nome" name="text" class="input is-large" placeholder="Nome" autofocus oninput="this.value = this.value.replace(/[^A-Za-z0-9]+/g, '').replace(/(\..*)\./g, '$1');">
+			</div>
+		</div>
+		
+		<div class="field">
+			<div class="control">
+				<strong>Seu Email:</strong>
+				<input name="email" class="input is-large" type="text" placeholder="Seu E-mail">
+		 	</div>
+		</div>
+		
+		<div class="field">
+			<div class="control">
+				<strong>Comentário:</strong>
+				<textarea style="height:10em;" name="comentario" class="input is-large" type="text" placeholder="Seu comentario" oninput="this.value = this.value.replace(/[^A-Za-z0-9]+/g, '').replace(/(\..*)\./g, '$1');"></textarea>
+			</div>
+		</div>
+		
+		<div class="field">
+			<a href="index.php"><strong>Cancelar</strong></a>
+		</div>
+		
+		<!-- Botão entrar com design CSS -->
+		<button type="submit" class="button is-dark is-block  is-large is-fullwidth">Enviar</button>
+	</form><br><br>
+				</div>
+			</div>
+		</div>
+	</div>
+	</section>
 	<?php 
-	// chamando o arquivo de cabeçalho
-	require 'rodape.php'; 
+		// chamando o arquivo de cabeçalho
+		require 'rodape.php'; 
 	?>
 </body>
 
