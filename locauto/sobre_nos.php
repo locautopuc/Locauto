d1ae01408b0001915ad018745291b024a3ec4014
Locauto <57334380+locautopuc@users.noreply.github.com>
2019-11-15 17:09:32 -0300

    Update sobre_nos.php

--- locauto/sobre_nos.php
+++ locauto/sobre_nos.php
@@ -11,36 +11,33 @@
     <link rel="stylesheet" href="css/bulma.min.css" />
     <link rel="stylesheet" type="text/css" href="css/config.css">
 </head>
-
-
 <body>
 	
 	
 	<?php 
-	// chamando o arquivo de cabeçalho
-	require 'cabecalho.php'; 
+		// chamando o arquivo de cabeçalho
+		require 'cabecalho.php'; 
 	?>
-	
-    <section class="hero is-success is-fullheight">
-        <div class="hero-body">
+
+	<section class="hero is-success is-fullheight">
+		<div class="hero-body">
 			<div class="container has-text-centered">
-                <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-grey">Locauto<strong>®</strong></h3>
-                    <h3 class="title has-text-grey"><a href="" target="_blank"> </a></h3>
-					
-					
-					<!-- Interface -->
-                    <div class="box"><strong>Sobre nós</strong><br><br>
-						A Locauto foi fundada em 2019 em GO pelos senhores Giulianno, Romario, Vinicius e Guilherme e a sua principal missão é facilitar o uso de automóveis para as pessoas que desejam alugar, em vez de comprar um veículo. Atualmente contamos com XX funcionários e os nossos principais clientes são XX. Esperamos que você se sinta confortável aqui.<br><br>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+				<div class="column is-4 is-offset-4">
+					<h3 class="title has-text-grey">Locauto<strong>®</strong></h3>
+					<h3 class="title has-text-grey"><a href="" target="_blank"> </a></h3>
+
+
+	<!-- Interface -->
+		<div class="box"><strong>Sobre nós</strong><br><br>
+			A Locauto foi fundada em 2019 em GO pelos senhores Giulianno, Romario, Vinicius e Guilherme e a sua principal missão é facilitar o uso de automóveis para as pessoas que desejam alugar, em vez de comprar um veículo. Atualmente contamos com XX funcionários e os nossos principais clientes são XX. Esperamos que você se sinta confortável aqui.<br><br>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 	<?php 
-	// chamando o arquivo de cabeçalho
-	require 'rodape.php'; 
+		// chamando o arquivo de cabeçalho
+		require 'rodape.php'; 
 	?>
 </body>
-
 </html>
