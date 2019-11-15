0099459515d37f2a935f62a5b71f1b234f2da1a5
locautopuc <57334380+locautopuc@users.noreply.github.com>
2019-11-15 15:12:24 -0300

    Add files via upload

--- locauto/index.php
+++ locauto/index.php
@@ -14,7 +14,7 @@ session_start();
     <title>Locauto</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="css/bulma.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <link rel="stylesheet" type="text/css" href="css/config.css">
 </head>
 
 
